08ba55b3d76b4fd1f08e91cfb2b76b08119d487d
rackerjoe <breu@breu.org>
2013-09-09 16:19:05 -0500

    Updating version # to 4.1.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache v2.0"
 description      "Manage modules on linux"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.14"
+version          "4.1.2"
 supports         "ubuntu"
 supports         "debian"
 name             "modules"
